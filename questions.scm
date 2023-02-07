aa2d05adc3005884b5dfc9174992b2ade55c4aae
osvaldo-valadez <osvaldo.valadez08@gmail.com>
2023-02-07 14:32:13 -0800

    Uploading Scheme Interpretor

---
+++ questions.scm
@@ -0,0 +1,74 @@
+(define (caar x) (car (car x)))
+(define (cadr x) (car (cdr x)))
+(define (cdar x) (cdr (car x)))
+(define (cddr x) (cdr (cdr x)))
+
+
+
+;; Problem 17
+;; Returns a list of two-element lists
+(define (enumerate s)
+  ; BEGIN PROBLEM 17
+  (define (helper s index)
+    (if (null? s)
+      nil
+      (cons (cons index (cons (car s) nil))
+            (helper (cdr s) (+ index 1)))))
+(helper s 0)
+  )
+  ; END PROBLEM 17
+
+;; Problem 18
+
+(define (zip pairs)
+  ; BEGIN PROBLEM 18
+  (cons (map (lambda (x) (car x)) pairs)
+    (cons (map (lambda (x) (car (cdr x))) pairs) nil)))
+  
+  ; END PROBLEM 18
+
+
+;; Problem 19
+;; Returns a function that checks if an expression is the special form FORM
+(define (check-special form)
+  (lambda (expr) (equal? form (car expr))))
+
+(define lambda? (check-special 'lambda))
+(define define? (check-special 'define))
+(define quoted? (check-special 'quote))
+(define let?    (check-special 'let))
+
+;; Converts all let special forms in EXPR into equivalent forms using lambda
+(define (let-to-lambda expr)
+  (cond ((atom? expr)
+         ; BEGIN PROBLEM 19
+         expr
+         ; END PROBLEM 19
+         )
+        ((quoted? expr)
+         ; BEGIN PROBLEM 19
+         expr
+         ; END PROBLEM 19
+         )
+        ((or (lambda? expr)
+             (define? expr))
+         (let ((form   (car expr))
+               (params (cadr expr))
+               (body   (cddr expr)))
+           ; BEGIN PROBLEM 19
+           (cons form (cons params (let-to-lambda body)))
+           ; END PROBLEM 19
+           ))
+        ((let? expr)
+         (let ((values (cadr expr))
+               (body   (cddr expr)))
+           ; BEGIN PROBLEM 19
+           (cons (cons 'lambda (cons (car (zip values)) (map let-to-lambda body)))
+              (map let-to-lambda (cadr(zip values))))
+           ; END PROBLEM 19
+           ))
+        (else
+         ; BEGIN PROBLEM 19
+         (cons (car expr) (map let-to-lambda (cdr expr)))
+         ; END PROBLEM 19
+         )))
